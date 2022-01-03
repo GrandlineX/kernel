2a7dc582de9675f6857fc5a4f6dd8f1de1d9fc0b
Elschnagoo <david.nagy@quinto.io>
2022-01-03 02:37:55 +0100

    v0.17.0

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@
  */
 import Kernel from './Kernel';
 import KernelModule from './KernelModule';
+import KernelDB from './database/KernelDB';
+import GKey from './database/entity/GKey';
 
 export * from './actions';
 export * from './api';
 export * from './classes';
 export * from './modules/crypto';
-import KernelDB from './database/KernelDB';
-import GKey from './database/entity/GKey';
 export * from './lib';
 export * from '@grandlinex/core';
 export * as bundlePostgreSQL from '@grandlinex/bundle-postgresql';
@@ -18,5 +18,5 @@ export * as bundleSQLight from '@grandlinex/bundle-sqlight';
 export * as bundleELogger from '@grandlinex/bundle-elogger';
 export { SQLCon } from '@grandlinex/bundle-sqlight';
 export { PGCon } from '@grandlinex/bundle-postgresql';
-export { KernelModule, Kernel ,KernelDB,GKey};
+export { KernelModule, Kernel, KernelDB, GKey };
 export default Kernel;
